5d46f747f80e7e983256d5012969b24e69374201
Deepak Rai <Deepakrai767622@gmail.com>
2020-03-13 14:23:41 +0530

    kuch nahi

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App" >
+        <div>
           <Header />
           <ScrollToTop>
             <Switch>
