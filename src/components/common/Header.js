7a1c01582594cd036a5bf94d5ff9c554e16b1bd4
Deepak Rai <Deepakrai767622@gmail.com>
2020-03-20 15:49:40 +0530

    office close

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,7 +8,6 @@ export class Header extends Component {
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-                <title>Welcome ! Deepak Rai Portfolio</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
                 {/* <link rel="stylesheet" href={process.env.PUBLIC_URL + "/style.css"} /> */}
                 {/* <link rel="stylesheet" href="public/style.css" /> */}
